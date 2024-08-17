b0839093b69359529794f1eec43c54f2f6792f53
Rod21 <trabajorod10@gmail.com>
2024-08-17 13:09:43 -0600

    Configuración correcta para wkhtmltopdf en Heroku

--- app.py
+++ app.py
@@ -45,8 +45,10 @@ def cargar_unidades_medicas():
 
 unidades_medicas_data = cargar_unidades_medicas()
 
-path_to_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'  # Reemplaza con la ruta correcta
+
+path_to_wkhtmltopdf = '/app/bin/wkhtmltopdf'
 config = pdfkit.configuration(wkhtmltopdf=path_to_wkhtmltopdf)
+
 # Uso de pdfkit con la configuración
 pdfkit.from_url('http://google.com', 'out.pdf', configuration=config)
 
